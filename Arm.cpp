c0ffb8ee08fa0b58596ce5274b23749ba46b7eaa
= <=>
2024-07-24 00:10:54 +0530

    working-arm

--- Arm.cpp
+++ Arm.cpp
@@ -1,47 +1,52 @@
 #include <random>
 #include <iostream>
+#include <vector> 
+
+// FIX: THIS MAKES MANY ARMS WITH THE SAME MEAN AND STDDEV, BUT REWARD WORKS PROPERLY 
 
 template <typename Reward>
 class Arm{
 protected: 
-    std::mt19937 generator;
+    std::mt19937* generatorptr;
 public: 
     Arm(std::mt19937& generator){
-        this->generator = generator;
+        this->generatorptr = &generator;
     }
     virtual Reward getReward() = 0;
 };
 
 class RandomGaussianArm: public Arm<double>{
-    double Mean;
-    double StdDev;
+    double mean;
+    double stdDev;
 public:
     RandomGaussianArm(std::mt19937& generator):Arm<double>(generator){
         std::cout<<"RandomGaussianArm constructor"<<"\t";
-        std::uniform_real_distribution<double> uniformGen(0,1);
-        this->Mean = uniformGen(this->generator);
-        this->StdDev = uniformGen(this->generator);
+        std::uniform_real_distribution<double> uniform(0,1);
+        this->mean = uniform(*(this->generatorptr));
+        this->stdDev = uniform(*(this->generatorptr));
         
-        std::cout<<"Mean: "<<this->Mean<<"\t";
-        std::cout<<"StdDev: "<<this->StdDev<<"\n";
+        std::cout<<"Mean: "<<this->mean<<"\t";
+        std::cout<<"StdDev: "<<this->stdDev<<"\n";
     };
     double getReward() override {
         std::cout<<"RandomGaussianArm getReward"<<"\t";
-        std::normal_distribution<double> gaussian(this->Mean,this->StdDev);
-        double reward = gaussian(this->generator);
+        std::normal_distribution<double> gaussian(this->mean,this->stdDev);
+        double reward = gaussian(*(this->generatorptr));
         std::cout<<"reward: "<<reward<<std::endl;
         return reward;
     }
 };
 
 int main(){
-    auto seed = static_cast<unsigned long>(std::time(0));
-    std::mt19937 generator(seed);
+    
+    unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
+    std::mt19937 generator(seed);    
     int ARMS = 3;
     int ROUNDS = 3;
     std::vector<RandomGaussianArm> arms; 
     for(int i = 0; i<ARMS; i++){
-        std::cout<<"Arm: "<<i<<"\n";
+
+        std::cout<<"Arm: "<<i+1<<"\n";
         arms.push_back(RandomGaussianArm(generator));
         for (int j = 0; j<ROUNDS; j++){
             arms[i].getReward();
